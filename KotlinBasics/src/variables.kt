110bd35f79e9c7576440304bf9260ef93b139714
MHO <mauricio.hernandz@ironbit.com.mx>
2017-10-19 17:58:33 -0500

    Variables y tipos en Kotlin.

---
+++ KotlinBasics/src/variables.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    val a = 5//Immutable
+    var b = 5
+
+    //a = 4//Not possible
+    b = 4
+
+    val name: String = "Mau"
+    //b = "a"//Not posible
+
+    b = "9".toInt() //Cast
+
+    val flt = 4f
+    val dbl = 23L
+}
