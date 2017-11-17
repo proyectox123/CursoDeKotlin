afed520081f8293a841e44ff421bf412e62a8797
MHO <mauricio.hdez.o.10@gmail.com>
2017-11-17 17:08:07 -0600

    Material Transitions conexión del item de la lista con la vista de detalle.

--- PlatziStore/app/src/main/java/com/platzistore/mho/platzistore/AdapterLanding.kt
+++ PlatziStore/app/src/main/java/com/platzistore/mho/platzistore/AdapterLanding.kt
@@ -1,10 +1,16 @@
 package com.platzistore.mho.platzistore
 
+import android.app.Activity
+import android.app.ActivityOptions
+import android.content.Intent
+import android.support.v4.app.ActivityOptionsCompat
+import android.support.v4.util.Pair
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import com.platzistore.mho.platzistore.model.ItemLanding
 import com.platzistore.mho.platzistore.utils.inflate
+import kotlinx.android.synthetic.main.activity_detail.view.*
 import kotlinx.android.synthetic.main.item_landing.view.*
 
 /**
@@ -27,6 +33,31 @@ class AdapterLanding(val data: List<ItemLanding>): RecyclerView.Adapter<AdapterL
                 itemView.txtTitleItem.text = title
                 itemView.txtTitleDesc.text = desc
                 itemView.txtTitlePrice.text = "$ ${String.format("%.2f", price)}"
+
+                itemView.setOnClickListener {
+                    val intent = Intent(itemView.context, DetailActivity::class.java)
+                    intent.putExtra("title", title)
+                    intent.putExtra("desc", desc)
+                    intent.putExtra("price", price)
+
+                    val pairHeader: Pair<View, String> =
+                            Pair.create(itemView.imageHeaderItem,
+                                    itemView.context.getString(R.string.transition_header))
+                    val pairTitle: Pair<View, String> =
+                            Pair.create(itemView.txtTitleItem,
+                                    itemView.context.getString(R.string.transition_title))
+                    val pairDesc: Pair<View, String> =
+                            Pair.create(itemView.txtTitleDesc,
+                                    itemView.context.getString(R.string.transition_desc))
+                    val pairPrice: Pair<View, String> =
+                            Pair.create(itemView.txtTitlePrice,
+                                    itemView.context.getString(R.string.transition_price))
+
+                    val options: ActivityOptionsCompat =
+                            ActivityOptionsCompat.makeSceneTransitionAnimation(itemView.context as Activity,
+                                    pairHeader, pairTitle, pairDesc, pairPrice)
+                    itemView.context.startActivity(intent, options.toBundle())
+                }
             }
         }
     }
--- PlatziStore/app/src/main/java/com/platzistore/mho/platzistore/DetailActivity.kt
+++ PlatziStore/app/src/main/java/com/platzistore/mho/platzistore/DetailActivity.kt
@@ -3,6 +3,7 @@ package com.platzistore.mho.platzistore
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_description.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -10,9 +11,10 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        intent?.let {
-            val textFromMain = it.extras.getString("any_text")
-            title_detail.text = textFromMain
+        intent?.extras?.let {
+            txtDetailTitle.text = it.getString("title")
+            txtDetailDesc.text = it.getString("desc")
+            txtDetailPrice.text = "$ ${String.format("%.2f", it.getDouble("price"))}"
         }
     }
 }
